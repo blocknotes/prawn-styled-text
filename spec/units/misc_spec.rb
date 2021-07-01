52e6f083b772c9b97745198c4439cbd4f9e54db1
Mattia Roccoberton <mat@blocknot.es>
2021-07-01 09:11:40 +0200

    Add misc spec

---
+++ spec/units/misc_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Misc' do
+  it 'renders some breaking line elements' do
+    html = 'First line<br>Second line<br/>Third line'
+    pdf = TestUtils.prepare_document(html)
+    text_analysis = PDF::Inspector::Text.analyze(pdf.render)
+
+    expect(text_analysis.strings).to match_array(['First line', 'Second line', 'Third line'])
+  end
+
+  it 'renders an horizontal line element', skip: 'TODO' do
+    pdf = TestUtils.prepare_document('<hr>Some content')
+    text_analysis = PDF::Inspector::Text.analyze(pdf.render)
+
+    expect(text_analysis.strings).to eq(['Some content'])
+    # text_analysis.positions # TODO: check text position
+  end
+end
